1de39f338ff973a76c3625834b4a8e74a45f9e6e
DonnyPHill <donald.p.hill@gmail.com>
2024-04-10 23:09:11 -0500

    Update app.py
    Completed to meet expectations.

--- app.py
+++ app.py
@@ -25,32 +25,48 @@ def balance_teams():
         team_number += 1
 
 def main_menu():
+    quit_app = 0
     while True:
-        print("---- MENU ----")
+        print("\n---- MENU ----\n")
         print("Here are your choices:")
         print(" 1)  Display Team Stats")
-        print(" 2)  Quit")
-        menu_choice = input("Enter an Option:  ")
-        if menu_choice == "1":
-            team_menu()
-        else:
+        print(" 2)  Quit\n")
+        while True:
+            menu_choice = input("Enter an Option:  ")
+            if menu_choice == "1":
+                team_menu()
+                break
+            elif menu_choice == "2":
+                quit_app = 1
+                break
+            else:
+                print("\nPlease press 1 to display team stats or 2 to quit\n")
+        if quit_app == 1:
             break
-    
+        
 def team_menu():
-    print(f"1) {teams[0]}")
+    print(f"\n1) {teams[0]}")
     print(f"2) {teams[1]}")
-    print(f"3) {teams[2]}")
-    team_choice = int(input("Enter an option: "))
+    print(f"3) {teams[2]}\n")
+    while True:
+        try:
+            team_choice = int(input("Enter an option: "))
+            if team_choice < 1 or team_choice > 3:
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print("\nPlease press 1, 2 or 3 for team stats\n")
     team_choice -= 1    
-    print(f"Team: {teams[team_choice]} Stats")
+    print(f"\nTeam: {teams[team_choice]} Stats")
     print("-" *len(f"Team: {teams[team_choice]} Stats"))
-    print(f"Total players: {players_per_team}")
+    print(f"Total players: {players_per_team}\n")
     print("Players on Team:")
     players_on_team = []
     for player in players:
         if player["team"] == team_choice:
             players_on_team.append(player["name"])    
-    print(f"  {", ".join(players_on_team)}")
+    print(f"  {", ".join(players_on_team)}\n")
     input("Press ENTER to continue...")
 
 
